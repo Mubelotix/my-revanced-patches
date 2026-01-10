0a7c39543f122f3f41f3e03e456425f366420285
Mubelotix <mubelotix@gmail.com>
2026-01-10 12:45:45 +0100

    Refactor

--- patches/src/main/kotlin/app/revanced/util/ReactNativePatch.kt
+++ patches/src/main/kotlin/app/revanced/util/ReactNativePatch.kt
@@ -1,15 +1,31 @@
 package app.revanced.util
 
-import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.BytecodePatchContext
+import app.revanced.patcher.patch.ResourcePatchContext
+import app.revanced.patcher.extensions.InstructionExtensions
 import com.android.tools.smali.dexlib2.Opcode
+import java.io.File
 
 object ReactNativePatch {
-    fun patch(context: BytecodePatchContext, jsCode: String, executeFirst: Boolean) {
-        context.resources.add("assets/revanced-plugin.js", jsCode)
+    fun installRawFile(context: ResourcePatchContext, jsCode: String) {
+        // Use ResourcePatchContext to write the file to assets
+        // 'true' typically means create/overwrite
+        val file = context.get("assets/revanced-plugin.js", true)
+        file.parentFile?.mkdirs()
+        file.writeText(jsCode)
+    }
 
-        val catalystInstanceImpl = context.classes.find { it.name == "Lcom/facebook/react/bridge/CatalystInstanceImpl;" } ?: return
+    fun patchBytecodeLoader(context: BytecodePatchContext, executeFirst: Boolean) {
+        val catalystInstanceImpl = context.classes.find { it.type == "Lcom/facebook/react/bridge/CatalystInstanceImpl;" } ?: return
         val runJSBundle = catalystInstanceImpl.methods.find { it.name == "runJSBundle" } ?: return
+        
+        val mutableClass = context.proxy(catalystInstanceImpl).mutableClass
+        // Find the mutable method corresponding to runJSBundle
+        val mutableMethod = mutableClass.methods.find { 
+            it.name == runJSBundle.name && 
+            it.returnType == runJSBundle.returnType && 
+            it.parameterTypes == runJSBundle.parameterTypes 
+        } ?: return
 
         // Smali code to load the script from assets
         // v0: AssetManager
@@ -26,20 +42,29 @@ object ReactNativePatch {
         """
 
         if (executeFirst) {
-            runJSBundle.addInstructions(0, loadScriptSmali)
+            with(InstructionExtensions) {
+                mutableMethod.addInstructions(0, loadScriptSmali)
+            }
         } else {
             // Find the index of the return-void instruction
-            val instructions = runJSBundle.implementation?.instructions ?: return
-            var returnIndex = -1
-            for ((index, instruction) in instructions.withIndex()) {
-                if (instruction.opcode == Opcode.RETURN_VOID) {
-                    returnIndex = index
+            // implementation is from MutableMethod (MethodImplementation)
+            val implementation = mutableMethod.implementation
+            if (implementation != null) {
+                val instructions = implementation.instructions
+                var returnIndex = -1
+                for ((index, instruction) in instructions.withIndex()) {
+                    if (instruction.opcode == Opcode.RETURN_VOID) {
+                        returnIndex = index
+                    }
                 }
-            }
 
-            if (returnIndex != -1) {
-                runJSBundle.addInstructions(returnIndex, loadScriptSmali)
+                if (returnIndex != -1) {
+                    with(InstructionExtensions) {
+                        mutableMethod.addInstructions(returnIndex, loadScriptSmali)
+                    }
+                }
             }
         }
     }
 }
+

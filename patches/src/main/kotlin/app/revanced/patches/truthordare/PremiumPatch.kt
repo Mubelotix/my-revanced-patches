a34e5c2dda0bb78edfccb5e872051ea615ba539c
Mubelotix <mubelotix@gmail.com>
2026-01-11 15:11:21 +0100

    Rename patch

--- patches/src/main/kotlin/app/revanced/patches/truthordare/PremiumPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/truthordare/PremiumPatch.kt
@@ -4,10 +4,10 @@ import app.revanced.patcher.patch.bytecodePatch
 import app.revanced.patches.reactnative.reactNativePatch
 
 @Suppress("unused")
-val debugPatch = bytecodePatch(
-    name = "Debug React Native",
+val premiumPatch = bytecodePatch(
+    name = "Activates premium features",
 ) {
-    // compatibleWith("com.antoinehabert.truthordategame"("2.9"));
+    compatibleWith("com.antoinehabert.truthordategame"("2.9"));
 
     // This patch depends on the generic React Native patch
     // which handles the injection.
